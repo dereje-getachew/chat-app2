f1b3dfec91a1695d923e267b40b5bdbd753e4196
derejegetachew12 <getachewdereje128@gmail.com>
2025-09-05 23:41:05 +0000

    error fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,9 @@ server.listen(PORT, () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-  app.get('*', (req, res) => {    
-    res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
-  });
+app.get('/:path(*)', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+});
+
 }
 
