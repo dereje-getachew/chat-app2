2883f1167e4e11e72f12792aa522ff1be89cd7f6
derejegetachew12 <getachewdereje128@gmail.com>
2025-09-07 15:33:59 +0000

    error

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,12 @@ app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('(.*)', (req, res) => {
+
+  app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
+
 
 
 server.listen(PORT, () => {
